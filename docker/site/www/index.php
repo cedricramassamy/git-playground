7f9070f797e71c8c3a894adbc58676ce2bcf9f90
Cédric Ramassamy <cedric.ramassamy@actelion.com>
2015-11-19 09:05:36 +0100

    [DOCKER] init

---
+++ docker/site/www/index.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>Test PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello docker enthusiasts</p>'; ?>
+ <?php echo '<p>This text is echoed by PHP.</p>'; ?>
+ </body>
+</html>
